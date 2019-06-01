dd3876d413cfd57242ccf55f6d87aa61422b1f32
Houdou <houdou.pt@gmail.com>
2019-06-01 14:01:47 +0800

    :bulb: Update info

--- preact.config.js
+++ preact.config.js
@@ -2,6 +2,8 @@ const { generateSw } = require('preact-cli-workbox-plugin');
 export default function(config, env, helpers) {
 	return generateSw(config, helpers, {
 		exclude: [
+			'/',
+			/bundle\.\w{5}\.esm.js/,
 			/route-info\.chunk\.\w{5}\.esm.js/,
 			/route-info\.chunk\.\w{5}\.css/,
 		],
