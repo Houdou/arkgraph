f8ef81e52ec55833325a40c90b1e40cf0c735478
Houdou <houdou.pt@gmail.com>
2020-04-21 12:29:50 +0800

    :wrench: Bump to 2.2.8

--- src/routes/info/index.js
+++ src/routes/info/index.js
@@ -16,6 +16,12 @@ const Info = ({
 			<h3 class={style.mobile_title}>明日方舟 | 干员培养表 <small>v{STORAGE_VERSION}</small></h3>
 			<hr />
 			<h2>{ir('homepage-announcement', '公告')}</h2>
+			<p>2020-04-21</p>
+			添加了新干员<br />
+			【<a href="/operator/傀影">傀影</a>】
+			【<a href="/operator/铸铁">铸铁</a>】
+			【<a href="/operator/巫恋">巫恋</a>】
+			【<a href="/operator/刻刀">刻刀</a>】
 			{
 				['en_US', 'ja_JP', 'ko_KR'].includes(config.locale) && (
 					<Fragment>
