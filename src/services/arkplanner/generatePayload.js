341bbcf3f17ca46ddd40b882b87462cb8013355d
Shirone <houdou.pt+bot@gmail.com>
2024-07-29 13:32:49 +0900

    :bug: Fix arkplanner export typing

--- src/services/arkplanner/generatePayload.js
+++ src/services/arkplanner/generatePayload.js
@@ -2,7 +2,7 @@ import { MATERIALS } from '../../models/Resources';
 
 const generateArkPlannerData = (summary, stock) => MATERIALS.map(
 	({ id, unique_id, name }) => ({
-		id: unique_id,
+		id: String(unique_id),
 		name,
 		need: summary[id] || 0,
 		have: stock[id] || 0,
